244eaeef3cd39cba5e738e682641cabc5ea8a4f1
theMVPshop <79482771+theMVPshop@users.noreply.github.com>
2022-05-29 15:47:59 -0500

    updated IG function

--- index.js
+++ index.js
@@ -1,36 +1,62 @@
 const fs = require('fs')
 const { createCanvas, loadImage } = require('canvas')
 
-const width = 1080
-const height = 1080
+let col1, col2, col3, response, noOfJobs;
+let search = ["Javascript", "HTML", "CSS", "Front End", "SQL", "React Developer", "Web Developer"];
+const d = new Date();
+let day = d.getDay();
+const axios = require('axios');
+const cheerio = require('cheerio');
 
-const canvas = createCanvas(width, height)
-const context = canvas.getContext('2d')
+(async () => {
+  const args = process.argv.slice(2);
+  const postCode = args[0] || 2000;
+  const url = `https://www.indeed.com/jobs?q=${search[day]}&l=USA&explvl=entry_level&fromage=1&remotejob=032b3046-06a3-4876-8dfd-474eb5e7ed11&vjk=1906e1b955a53d6e`;
+  try {
+    const response = await axios.get(url);
+    const $ = cheerio.load(response.data);
+    noOfJobs = $("#searchCountPages").text().trim().split(" ")[3];
+    ig();
+  } catch (e) {
+    console.error(`Error while fetching rental properties for ${postCode} - ${e.message}`);
+  }
+})();
 
-loadImage('bkgd/0'+(Math.floor(Math.random()*9)+1)+'.jpg').then(image => {
-    // sx,sy,swidth,sheight,x,y,width,height
-    context.drawImage(image, Math.floor(Math.random()*3920), Math.floor(Math.random()*3920), width, height, 0, 0, width, height)
+
+function ig() {
+  const width = 1080
+  const height = 1080
+
+  const canvas = createCanvas(width, height)
+  const context = canvas.getContext('2d')
+
+  loadImage('bkgd/0'+(Math.floor(Math.random()*9)+1)+'.jpg').then(image => {
+      // sx,sy,swidth,sheight,x,y,width,height
+      context.drawImage(image, Math.floor(Math.random()*3220), Math.floor(Math.random()*3220), width, height, 0, 0, width, height)
+    // })
+
+  context.font = 'bold 180pt Menlo'
+  context.textAlign = 'center'
+  context.textBaseline = 'top'
+  context.fillStyle = '#00F'
+  context.fillText(noOfJobs, width/2, 30);
+  context.font = 'bold 60pt Menlo'
+  context.fillStyle = '#000'
+  context.fillText("Remote, Entry Level", width/2, 300);
+  context.font = 'bold 70pt Menlo'
+  context.fillStyle = '#00F'
+  context.fillText(search[day]+" Jobs", width/2, 400);
+  context.font = 'bold 60pt Menlo'
+  context.fillStyle = '#000'
+  context.fillText("Were Posted on", width/2, 525);
+  context.font = 'bold 60pt Menlo'
+  context.fillText("In The Last 24 Hours", width/2, 920);
+  context.font = 'bold 25pt Menlo'
+  context.fillText("(Click the Link in the Description)", width/2, 1020);
+  loadImage('./logo.png').then(image => {
+    context.drawImage(image, 78, 650, 923, 239)
+    const buffer = canvas.toBuffer('image/png')
+    fs.writeFileSync('./ig.png', buffer)
   })
-
-context.font = 'bold 70pt Menlo'
-context.textAlign = 'center'
-context.textBaseline = 'top'
-context.fillStyle = '#000'
-
-const text = 'Hello, World!'
-
-const textWidth = context.measureText(text).width
-// context.fillRect(width - textWidth / 2 - 10, 170 - 5, textWidth + 20, 120)
-// context.fillStyle = '#000'
-// console.log((width - textWidth) / 2);
-context.fillText(text, 0, 170)
-
-// context.fillStyle = '#000'
-// context.font = 'bold 30pt Menlo'
-// context.fillText('flaviocopes.com', 600, 530)
-
-loadImage('./logo.png').then(image => {
-  context.drawImage(image, 78, 515, 923, 239)
-  const buffer = canvas.toBuffer('image/png')
-  fs.writeFileSync('./ig.png', buffer)
-})
+  })
+}
